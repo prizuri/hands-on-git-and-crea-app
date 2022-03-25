af47552c8c84de6da37029d629f2b2334d79c646
Prizuri Hartadi <prizurihartadi@gmail.com>
2022-03-25 09:49:52 +0700

    Add input and search button, and a gif

---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Giphy(){
+    return(
+        <div>
+            <form>
+                <input type="text" placeholder="Search..."/>
+                <button type="submit">Search</button>
+            </form>
+            <img src="https://media0.giphy.com/media/Vh8pbGX3SGRwFDh3V0/giphy.gif?cid=790b7611b7808c3eb867b652ba77301e5e21465250fb137e&rid=giphy.gif&ct=g"/>
+        </div>
+    )
+}
