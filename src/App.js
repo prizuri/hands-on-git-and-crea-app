a1c8bc9f561d3ac1a66e2a62fdf1119dd45180f7
Prizuri Hartadi <prizurihartadi@gmail.com>
2022-03-31 20:46:45 +0700

    Add search input, fetch api, and use React Hooks to change and use state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import './App.css';
-import Form from './components/form';
-import data from './data/data';
-import Gif from './components/gif';
+import React from "react"
+import axios from "axios"
 
 function App() {
-  const filteredData = data.filter((value=> value.rating!=="g"))
-  const getData = filteredData.map(data=>{
-    return <Gif key={data.id} url={data.url} title={data.title}/>
-  })
+  const [query, setQuery] = React.useState("")
+  const [dataGif, setDataGif] = React.useState([])
+  console.log("Rendering")
+
+  //Use effect will run in every changes query
+  React.useEffect(()=>{
+    console.log("Use effect")
+    fetchingDataGif()
+  }, [query])
+
+  //Fetching API with axios and save the changes to setDataGif
+  function fetchingDataGif(){
+    axios.get(`https://api.giphy.com/v1/gifs/search?&api_key=u9n8dbJfa0HMqK4KaEwaL9pxkbzfUTBY&q=${query}&limit=12`)
+      .then(response => {
+        setDataGif(response.data.data)
+      })
+      .catch(error => alert(error))
+  }
+
+  function handleInput(event){
+    setQuery(event.target.value)
+  }
+
   return (
     <div className="App">
-      <Form />
-      {getData}
+      <form>
+        <input type="text" onChange={handleInput}/>
+        <button>Submit</button>
+      </form>
+      
+      {dataGif.map(data=>{
+        return (<img key={data.id} src={data.images.fixed_width.url}/>)
+        
+      })}
     </div>
   );
 }
